2966205e6e9b5ba09ab356806dd7f6bd7d45480b
Michael50234 <michael.shi7m@gmail.com>
2026-02-14 23:50:04 -0500

    Built functions for getting cropped image details and generating the cropped image itself

--- components/crop/imageCropper.tsx
+++ components/crop/imageCropper.tsx
@@ -2,14 +2,63 @@
 
 import { Box, Button, IconButton } from '@mui/material';
 import style from './ImageCropper.module.css'
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import CloseIcon from '@mui/icons-material/Close'
 import Cropper from 'react-easy-crop';
+
+
+//Create cropped image and then save to firebase storage
+//Create save button which toggles disabled state
 
 const ImageCropper = ({handleDialogClose}: {handleDialogClose: () => void}) => {
     const [image, setImage] = useState<null | string>(null);
     const [crop, setCrop] = useState({x: 0, y: 0});
     const [zoom, setZoom] = useState(1);
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState<Record<string, number> | null>(null);
+
+    const getCroppedImage = (imageSrc: string, cropArea: Record<string, number>) => {
+        return new Promise((resolve) => {
+            const image = new Image();
+            image.src = imageSrc;
+
+
+            image.onload = () => {
+                //Creates a canvas
+                const canvas = document.createElement("canvas");
+                canvas.width = cropArea['width'];
+                canvas.height = cropArea['height'];
+                
+                //Ctx is used to draw on the canvas
+                const ctx = canvas.getContext("2d");
+
+                if(!ctx){
+                    return;
+                }
+                
+                //Draws the cropped image onto the canvas
+                ctx.drawImage(
+                    image, 
+                    cropArea.x,
+                    cropArea.y,
+                    cropArea.width,
+                    cropArea.height,
+                    0,
+                    0,
+                    cropArea.width,
+                    cropArea.height
+                )
+                
+                //Takes whatever is on the canvas and turns it into an image format then a blob
+                canvas.toBlob((blob) => {
+                    resolve(blob);
+                }, "image/jpeg")
+            }
+        })
+    }
+
+    const onCropComplete = useCallback((_, croppedPixels: Record<string, number>) => {
+        setCroppedAreaPixels(croppedPixels);
+    }, [])
 
 
     const onSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -46,6 +95,7 @@ const ImageCropper = ({handleDialogClose}: {handleDialogClose: () => void}) => {
                         onChange={onSelectFile}
                     ></input>
                 </Button>
+                <Button>Save Image</Button>
                 <IconButton onClick={handleDialogClose}>
                     <CloseIcon />
                 </IconButton>
@@ -62,6 +112,7 @@ const ImageCropper = ({handleDialogClose}: {handleDialogClose: () => void}) => {
                         zoom={zoom}
                         onCropChange={setCrop}
                         onZoomChange={setZoom}
+                        onCropComplete={onCropComplete}
                     >
 
                     
